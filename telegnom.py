02007f71b88d96e86723617648951eb3f625a288
RETolia <81373842+RETolia@users.noreply.github.com>
2025-04-17 00:26:44 +0300

    Add files via upload

--- telegnom.py
+++ telegnom.py
@@ -1,6 +1,7 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 import asyncio
+from aiogram.enums import ChatType
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import WebAppInfo
@@ -14,6 +15,20 @@ async def start_cmd(message: types.Message):
 async def main():
     await dp.start_polling(bot)
 
+
+def is_allowed_chat(message: types.Message) -> bool:
+    return (
+        message.chat.type == ChatType.PRIVATE or  # Личные сообщения
+        message.message_thread_id is None  # General (не топик)
+    )
+
+
+
+
+
+
+
+
 #----------------------------Reply-клавиатура:
 def get_reply_kb():
     builder = ReplyKeyboardBuilder()
@@ -22,7 +37,7 @@ def get_reply_kb():
     builder.adjust(2)
     return builder.as_markup(resize_keyboard=True)
 
-@dp.message(Command("keyboard"))
+@dp.message(Command("keyboard"), is_allowed_chat)
 async def kb_cmd(message: types.Message):
     await message.answer("Выберите:", reply_markup=get_reply_kb())
 
@@ -36,13 +51,13 @@ def get_inline_kb():
 async def process_callback(callback: types.CallbackQuery):
     await callback.answer("Вы нажали кнопку!", show_alert=True)
 
-@dp.message(Command("inline"))
+@dp.message(Command("inline"), is_allowed_chat)
 async def show_inline_kb(message: types.Message):
     await message.answer("Пример inline-клавиатуры:", reply_markup=get_inline_kb())
 
 
 #----------------------------отправка фотопо ссылке:
-@dp.message(Command("photo"))
+@dp.message(Command("photo"), is_allowed_chat)
 async def send_photo(message: types.Message):
     await message.answer_photo(
         photo="https://images.app.goo.gl/iTFdnTdK4SNGNwHMA",
@@ -58,8 +73,22 @@ def get_game_keyboard():
     )
     return builder.as_markup()
 
+    
+@dp.message(Command("hi"), is_allowed_chat)
+async def hi(message: types.Message):
+    await message.answer("hi")
+
+@dp.message(Command("get_thread_id"))
+async def get_thread_id(message: types.Message):
+    # Получаем ID топика (если сообщение отправлено в топике)
+    thread_id = message.message_thread_id
+    
+    if thread_id:
+        await message.reply(f"ID этого топика: {thread_id}")
+    else:
+        await message.reply("Это сообщение не в топике! Отправьте команду в нужном подчате.")
 # Команда для запуска
-@dp.message(Command("game"))
+@dp.message(Command("game"), is_allowed_chat)
 async def cmd_game(message: types.Message):
     await message.answer(
         "Нажмите кнопку, чтобы открыть игру:",
